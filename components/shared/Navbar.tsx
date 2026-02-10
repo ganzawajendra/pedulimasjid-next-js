200d9e7f831dc3fa6de7bb557b881999306a56d6
ganzawajendra <ganzawjendra@gmail.com>
2026-02-10 12:28:54 +0700

    feat(Navbar): Membenarkan responsive di Navbar

--- components/shared/Navbar.tsx
+++ components/shared/Navbar.tsx
@@ -5,10 +5,10 @@ import LogoPeduliMasjid from '@/public/logo-pedulimasjid-putih.svg';
 
 export default function Navbar() {
     return(
-        <header id="navbar" className="flex justify-between items-center ">
-          <div>
+        <header id="navbar" className="flex justify-between items-center h-6 xs:h-6 sm:h-8 md:h-10 lg:h-10">
+          <Link href={'/'}>
             <Image src={LogoPeduliMasjid} alt="Logo Peduli Masjid" className="w-15 lg:w-30 md:w-20 sm:w-20 xs:w-15 "/>
-          </div>
+          </Link>
           <div>
             <Link href={'/login'}>
               <Button variant="primary" className="px-3 py-1">Login</Button>
